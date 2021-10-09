5a1a0c963685df7df75eff57d03f188770e5686c
Yana <yanaegorova69.3@gmail.com>
2021-10-09 19:22:31 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,140 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Silicone Case',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Watch protective cover is made of high-quality soft and flexible silicone, which is safty and comfortable.',
+            },
+            {
+                text: '42mm silicone protective case can protect your watch from water, dirt, scratch, and keep your watch in a good condition for a longer time.',
+            },
+            {
+                text: 'You can choose different colors to change your watch\'s color to give your different usage\'s experience in daily life.',
+            },
+            {
+                text: '42mm silicone case is easy to install and remove.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Closet Storage Organizer',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Free-standing closet organizer with 3 fabric shelves and 2 collapsible fabric drawers with handles.',
+            },
+            {
+                text: 'Each shelf can hold up to 10 pounds; each drawer can hold up to 5 pounds; stylish, breathable dark-gray fabric.',
+            },
+            {
+                text: 'Powder-coated steel frame; metal rods offer reliable support; overall design ensures stability.',
+            },
+            {
+                text: 'Lightweight at just 7.1 pounds; wipes clean with damp cloth; easy to assemble—no tools required.',
+            },
+            {
+                text: 'Measures 32.7 by 12.2 by 31 inches (LxWxH); weighs 7.1 pounds.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Floor Lamp',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Constructed of Metal.',
+            },
+            {
+                text: '3-Way Rotary Switch.',
+            },
+            {
+                text: 'Requires (1) 150 watt bulb, not included.',
+            },
+            {
+                text: 'Metal Ball Accent.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Adjustable Bed Base',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Adjustable bed base for automatically changing comfort positions.',
+            },
+            {
+                text: 'Separately adjust head and foot elevation with included wireless remote control.',
+            },
+            {
+                text: 'Ideal for sleeping, watching television or movies, working on your laptop, or reading.',
+            },
+            {
+                text: 'Designed for use with adjustable, foam, latex, or air mattresses.',
+            },
+            {
+                text: 'Easy assembly; hardware included.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Kids Table & Chair Set',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Kids’ table for playing, eating, reading, and more.',
+            },
+            {
+                text: 'Perfect height and size for children.',
+            },
+            {
+                text: 'Includes square table and four matching chairs.',
+            },
+            {
+                text: 'Made of durable, natural solid wood.',
+            },
+            {
+                text: 'Dynamic use in playrooms, bedrooms, kitchens, living rooms, or outdoors.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Upright Vacuums',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: '400 watts of power. Built-in LEDS: Spot fine hair and dust in dark areas.',
+            },
+            {
+                text: '3 power settings: Low, normal, high. Long battery life: Low speed 45mins. Normal speed 20mins. High speed 8mins.',
+            },
+            {
+                text: 'Wall mounted: The included wall mount keeps the vacuum stored safely off the ground.',
+            },
+            {
+                text: 'Easy maintenance: Simply touch the release button to remove and empty the dustbin, so you can get right back to cleaning.',
+            },
+            {
+                text: 'Attachments for every cleaning situation: The four included attachments allow for simple and straightforward cleaning. Attachments are included for every situation, from vacuuming the floor of your car to cobwebs in hard to reach corners and everything in between.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png','./img/img6-5.png'],
+    },
+]
